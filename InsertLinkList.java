392731d14f34928680dbd9ff25e1c07f05fa70b6
Madhu Sudhan <m02sudhan@gmail.com>
2022-10-13 18:15:04 +0530

    Inserting the element

---
+++ InsertLinkList.java
@@ -0,0 +1,78 @@
+public class InsertLinkList <T> {
+    Node head, tail;
+    static class Node <T>{
+
+        T data;
+        Node next;
+
+        Node(T d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+
+    public  void append(int data)
+    {
+
+        Node new_node = new Node(data);
+
+        if (head == null) {
+            head = new_node;
+        }
+        else {
+
+            Node last = head;
+            while (last.next != null) {
+                last = last.next;
+            }
+
+
+            last.next = new_node;
+        }
+
+    }
+
+
+    public  void printList(InsertLinkList list)
+    {
+        Node currNode = list.head;
+
+        System.out.print("LinkedList: ");
+
+        while (currNode != null) {
+
+            System.out.print(currNode.data + " ");
+
+
+            currNode = currNode.next;
+        }
+    }
+
+    public  void insert(T data) {
+        Node<T> newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            Node<T> temp, current;
+            current = head;
+            temp = head.next;
+            current.next = newNode;
+            newNode.next = temp;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+
+        InsertLinkList<Integer> list = new InsertLinkList();
+
+
+        list.append(56);
+        list.append(70);
+        list.insert(30);
+        list.printList(list);
+    }
+}
